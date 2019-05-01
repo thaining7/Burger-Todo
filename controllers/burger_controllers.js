0cbba14006b7442fd57983922990d6451269a76f
thaining7 <thaining@protonmail.com>
2019-05-01 15:57:32 -0500

    working first rough draft

--- controllers/burger_controllers.js
+++ controllers/burger_controllers.js
@@ -18,7 +18,7 @@ router.post("/api/burgers", function (req, res) {
     burger.insertOne([
         "burger_name", "devoured"
     ], [
-            req.body.burger_name
+            req.body.burger_name, false
         ], function (result) {
             // Send back the ID of the new quote
             res.json({ id: result.insertId });
@@ -31,7 +31,7 @@ router.put("/api/burgers/:id", function (req, res) {
     console.log("condition", condition);
 
     burger.updateOne({
-        devoured: req.body.devoured
+        devoured: true
     }, condition, function (result) {
         if (result.changedRows == 0) {
             // If no rows were changed, then the ID must not exist, so 404
