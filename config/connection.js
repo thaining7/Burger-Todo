7e8554e2d8070f425c057841ef1c78acdfbcc7c5
thaining7 <thaining@protonmail.com>
2019-05-03 10:10:35 -0500

    fixed jawsdb connection

--- config/connection.js
+++ config/connection.js
@@ -13,13 +13,14 @@ if (process.env.JAWSDB_URL) {
         database: "burgers_db"
 
     });
+};
 
-    connection.connect(function (err) {
-        if (err) {
-            console.error("error connecting: " + err.stack);
-            return;
-        }
-        console.log("connected as id " + connection.threadId);
-    });
+connection.connect(function (err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
+});
 
-    module.exports = connection;
+module.exports = connection;
