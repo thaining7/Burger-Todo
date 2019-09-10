8da3767041fafdc6aa733f7b2610f680cf9519a4
thaining7 <thaining@protonmail.com>
2019-09-10 12:03:35 -0500

    added app screenshot

--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,9 @@
+drop database if exists burgers_db;
 create database burgers_db;
 use burgers_db;
 create table burgers (
     id int not null auto_increment,
     burger_name varchar(30),
-    devoured boolean
+    devoured boolean,
+    primary key (id)
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -3,3 +3,5 @@ insert into burgers (burger_name, devoured)
 values ("Bacon Cheeseburger", false),
 ("Big Mick", false),
 ("Three Eighths Pounder", false);
+
+select * from burgers;
